b03cd10575ea804ff786d146a1d9620693f69324
jenny-han <dev.jennyhan@gmail.com>
2023-06-17 16:34:22 +0900

    back setting

---
+++ back/WebService/src/test/java/com/jnhn/WebService/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnhn.WebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
