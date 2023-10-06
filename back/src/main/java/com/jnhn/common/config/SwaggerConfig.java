3517400ed607df575ed2d754d3d16d01a17c5b1c
jenny-han <dev.jennyhan@gmail.com>
2023-10-06 23:32:30 +0900

    FIX : spring swagger error fix

---
+++ back/src/main/java/com/jnhn/common/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.jnhn.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+	public OpenAPI restAPI() {
+		return new OpenAPI()
+			 .components(new Components())
+            .info(apiInfo());
+	}
+
+	private Info apiInfo() {
+		return new Info()
+				.title("marrycard web Spring Boot REST API")
+				.version("1.0.0")
+				.description("marry card web swagger api.");
+	}
+}
