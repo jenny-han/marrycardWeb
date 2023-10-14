fadb1d73d9efbbe156117d76b583acc870307740
jenny-han <dev.jennyhan@gmail.com>
2023-10-14 23:57:09 +0900

    ADD : set theme color and web font

---
+++ front/src/lib/styles/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#a7b197',
+      main: '#919e7d',
+      dark: '#656e57',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffd453',
+      main: '#ffca28',
+      dark: '#b28d1c',
+      contrastText: '#000',
+    },
+  },
+})
