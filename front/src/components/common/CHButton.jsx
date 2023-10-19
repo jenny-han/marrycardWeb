f4f095f82672eac6dec41ce291e7b6d9e60e2d64
jenny-han <dev.jennyhan@gmail.com>
2023-10-20 00:43:17 +0900

    ADD : CHButton disable option

--- front/src/components/common/CHButton.jsx
+++ front/src/components/common/CHButton.jsx
@@ -1,17 +1,26 @@
 import { Button } from '@mui/material'
+import { useEffect, useState } from 'react'
 
 const CHButton = (props) => {
-  const { variant, color, text, onClickListener, disabled, fullWidth } = props
-  // TODO : color setting은 좀더 편하게 불러올수 있도록 수정 보완하면 좋을듯.
-  // 어떤 기능에 어떤 컬러를 줄건지 생각해보기.
-  //style width 100% 인 버튼 만들기?
+  const { variant, color, text, onClick, disabled, fullWidth } = props
+  // TODO : color setting은 좀더 편하게 불러올 수 있도록 해도 좋을 듯
+  // 어떤 기능에 어떤 색상을 줄건지 생각해보기
+  // fullWidth true => style width 100%
+
+  const [disable, setDisable] = useState(false)
+
+  useEffect(() => {
+    // 특정 요청을 했을때 버튼을 disabled 처리
+    if (text === 'SAVE') setDisable(true)
+    else setDisable(disabled)
+  })
 
   return (
     <Button
       variant={variant}
       color={color}
-      disabled={disabled}
-      onClick={onClickListener}
+      disabled={disable}
+      onClick={onClick}
       style={{ width: fullWidth ? '100%' : 'unset' }}
     >
       {text}
