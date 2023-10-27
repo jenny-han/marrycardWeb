dbdf5e046b436a235d8b3fd725b2a117b0ec8b5c
jenny-han <dev.jennyhan@gmail.com>
2023-10-28 00:22:48 +0900

    feat : share link components

--- front/src/components/preview/ShareLink.jsx
+++ front/src/components/preview/ShareLink.jsx
@@ -1,9 +1,49 @@
 import styled from 'styled-components'
+import { copyDoc } from '../../modules/copy'
+import CardTitleText from '../common/CardTitleText'
+import { sendMessage } from '../../modules/KaKaoMessageScript'
+import LinkIcon from '@mui/icons-material/Link'
 
-const ShareLinkBlock = styled.div``
+import kakao from '../../lib/img/kakao-talk-lightbrown.png'
+import link from '../../lib/img/link-lightbrown.png'
+
+const ShareLinkBlock = styled.div`
+  text-align: center;
+  padding: 7vw 2vw;
+`
+const ShareBox = styled.div`
+  margin-top: 5vw;
+  width: 35%;
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+`
+const ShareBtn = styled.div`
+  img {
+    width: 50px;
+  }
+`
 
 const ShareLink = () => {
-  return <ShareLinkBlock></ShareLinkBlock>
+  const doCopyLink = () => {
+    const url = 'https://ch-wedding.kr/card/jyhs1209'
+    copyDoc('청첩장 링크', url)
+  }
+  return (
+    <ShareLinkBlock>
+      <CardTitleText text="청첩장 공유하기" />
+      <ShareBox>
+        <ShareBtn onClick={sendMessage}>
+          {/* 카카오톡공유 */}
+          <img src={kakao} alt="kakao" />
+        </ShareBtn>
+        <ShareBtn onClick={doCopyLink}>
+          {/* 링크복사 */}
+          <img src={link} alt="link" />
+        </ShareBtn>
+      </ShareBox>
+    </ShareLinkBlock>
+  )
 }
 
 export default ShareLink
